656b2cd4e96a0e6f70e09cf67d18d76e89fe9f8b
rorvts <rorvts@gmail.com>
2020-08-19 18:33:22 +0200

    CRUD

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,37 @@
 const express = require("express");
 
-const app = express();
+const server = express();
+server.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({message: "test"});
+
+const users = ["UserOne", "UserTwo", "UserThree"];
+
+server.get("/users/:index", (request, response) => {
+  const { index } = request.params;
+  return response.json(users[index]);
 });
 
-app.listen("3333");
+server.get("/users", (request, response) => {
+  return response.json(users)
+});
+
+server.post("/users", (request, response) => {
+  const { name } = request.body;
+  users.push(name);
+  return response.json(users);
+});
+
+server.put("/users/:index", (request, response) => {
+  const { index } = request.params;
+  const { name } = request.body;
+  users[index] = name;
+  return response.json(users);
+});
+
+server.delete("/users/:index", (request, response) => {
+  const { index } = request.params;
+  users.splice(index, 1);
+  return response.json(users);
+});
+
+server.listen("3333");
