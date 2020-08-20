d24cb4e85c145509cc19cc8e7c1eaefd8dc1324c
rorvts <rorvts@gmail.com>
2020-08-20 12:30:52 +0200

    Loca middleware and modifying request params

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,29 +13,47 @@ server.use((request, response, next) => {
   console.timeEnd("Request");
 });
 
-server.get("/users/:index", (request, response) => {
-  const { index } = request.params;
-  return response.json(users[index]);
+function checkUserParam(request, response, next){
+  if(!request.body.name){
+    return response.status(400).json({ error: "User name is required."});
+  }
+  return next();
+}
+
+function checkUserExists(request, response, next){
+  const user = users[request.params.index];
+  
+  if(!user){
+    return response.status(400).json({ error: "User doesn't exists."});
+  }
+
+  request.user = user;
+
+  return next();
+}
+
+server.get("/users/:index", checkUserExists, (request, response) => {
+  return response.json(request.user);
 });
 
 server.get("/users", (request, response) => {
   return response.json(users)
 });
 
-server.post("/users", (request, response) => {
+server.post("/users", checkUserParam, (request, response) => {
   const { name } = request.body;
   users.push(name);
   return response.json(users);
 });
 
-server.put("/users/:index", (request, response) => {
+server.put("/users/:index", checkUserParam, checkUserExists, (request, response) => {
   const { index } = request.params;
   const { name } = request.body;
   users[index] = name;
   return response.json(users);
 });
 
-server.delete("/users/:index", (request, response) => {
+server.delete("/users/:index", checkUserExists, (request, response) => {
   const { index } = request.params;
   users.splice(index, 1);
   return response.json(users);
