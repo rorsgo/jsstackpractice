a9c4d43a7a9484eb69579e392ca544c6df13a51b
rorvts <rorvts@gmail.com>
2020-08-21 23:30:55 +0200

    database configuration file

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get("/", (request, response) => {
-  return response.json({message: "test"});
+routes.get("/", async (request, response) => {
+  const user = await User.create({
+    name: "User",
+    email: "example@email.com",
+    password_hash: "hash1@example@1hash",
+  });
+  return response.json(user);
 });
 
 export default routes;
