fb0cc1c280e43a1bc1e5452ae88d2701936ea904
rorvts <rorvts@gmail.com>
2020-08-27 14:46:48 +0200

    UserController Yup schema validation

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from "yup";
 import User from "../models/User";
 
 class UserController {
   async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6)
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: "Fields not valid." });
+    }
+
     const userExists = await User.findOne({ where: { email: request.body.email } });
     if (userExists) {
       return response.status(400).json({ error: "Email address already registered." });
@@ -16,9 +27,26 @@ class UserController {
   }
 
   async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string().min(6)
+        .when("oldPassword", (oldPassword, password) =>
+          oldPassword ? password.required() : password
+        ),
+      passwordConfirmation: Yup.string()
+        .when("password", (password, passwordConfirmation) =>
+          password ? passwordConfirmation.required().oneOf([Yup.ref("password")]) : passwordConfirmation
+        ),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: "Fields not valid." });
+    }
+
     const { email, oldPassword } = request.body;
     const user = await User.findByPk(request.userId);
-    console.log(user);
 
     if (email !== user.email) {
       const userExists = await User.findOne({ where: { email } });
