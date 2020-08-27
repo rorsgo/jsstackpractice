b4211619203516905415083ad3a588c97c2a8a50
rorvts <rorvts@gmail.com>
2020-08-27 21:11:47 +0200

    FileController config and routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,15 +6,14 @@ import authMiddleware from "./app/middlewares/auth";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import FileController from "./app/controllers/FileController";
 
 const routes = new Router();
 const uploads = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
-routes.post("/files", uploads.single("file"), (request, response) => {
-  return response.json({ ok: true });
-});
+routes.post("/files", uploads.single("file"), FileController.store);
 
 routes.use(authMiddleware);
 
