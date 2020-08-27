79bbfab9ca0dc482fce7506bf15dd0d62f6882d2
rorvts <rorvts@gmail.com>
2020-08-27 15:21:28 +0200

    Multer image upload

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
+import multer from "multer";
 
+import multerConfig from "./config/multer";
 import authMiddleware from "./app/middlewares/auth";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 
 const routes = new Router();
+const uploads = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
+routes.post("/files", uploads.single("file"), (request, response) => {
+  return response.json({ ok: true });
+});
 
 routes.use(authMiddleware);
 
