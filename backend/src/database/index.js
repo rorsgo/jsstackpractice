73ca6f4cbf9db40c323eedadf6a775601f37017f
rorvts <rorvts@gmail.com>
2020-08-28 14:16:14 +0200

    Appointment migration and model

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,10 +1,12 @@
 import Sequelize from "sequelize";
 
-import User from "../app/models/User";
-import File from "../app/models/File";
 import databaseConfig from "../config/database";
 
-const models = [User, File];
+import User from "../app/models/User";
+import File from "../app/models/File";
+import Appointment from "../app/models/Appointment";
+
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
---
+++ backend/src/database/migrations/20200828115336-create-appointments.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("appointments", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: "users", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+        allowNull: false
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: "users", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+        allowNull: false
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("appointments");
+  }
+};
