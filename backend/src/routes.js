69d5d75ece7b693c421ccc89aa4b0098f07f7182
rorvts <rorvts@gmail.com>
2020-08-28 12:02:28 +0200

    ProviderController method index and routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,9 +7,12 @@ import authMiddleware from "./app/middlewares/auth";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
+import ProviderController from "./app/controllers/ProviderController";
 
 const routes = new Router();
 const uploads = multer(multerConfig);
+
+routes.get("/providers", ProviderController.index);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
