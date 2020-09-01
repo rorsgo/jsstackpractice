1231a1f9fdcc76a0da280b1f52dc09ad93260b8e
rorvts <rorvts@gmail.com>
2020-09-01 13:17:21 +0200

    Verifying appointment dates | before dates or already registered appointments

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,5 +1,7 @@
 import Appointment from "../models/Appointment";
 import User from "../models/User";
+
+import { isBefore, parseISO, startOfHour } from "date-fns";
 import * as Yup from "yup";
 
 class AppointmentController {
@@ -22,6 +24,24 @@ class AppointmentController {
       return response.status(401).json({ error: "You can only create appointments with providers."});
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    if(isBefore(hourStart, new Date())){
+      return response.status(400).json({ error: "Past dates are not allowed."});
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    });
+
+    if(checkAvailability){
+      response.status(400).json({ error: "Appointment date is not available."});
+    }
+    
     const appointment = await Appointment.create({
       user_id: request.userId,
       provider_id,
