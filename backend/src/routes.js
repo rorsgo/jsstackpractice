44c8e08d456271db96f5b238dc04bb042dc90a81
rorvts <rorvts@gmail.com>
2020-09-01 13:16:12 +0200

    Apllying auth middleware in specific routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -13,15 +13,13 @@ import AppointmentController from "./app/controllers/AppointmentController";
 const routes = new Router();
 const uploads = multer(multerConfig);
 
-routes.use(authMiddleware);
-
-routes.get("/providers", ProviderController.index);
+routes.get("/providers", authMiddleware, ProviderController.index);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
-routes.post("/files", uploads.single("file"), FileController.store);
-routes.post("/appointments", AppointmentController.store);
+routes.post("/files", authMiddleware, uploads.single("file"), FileController.store);
+routes.post("/appointments", authMiddleware, AppointmentController.store);
 
-routes.put("/users", UserController.update);
+routes.put("/users", authMiddleware, UserController.update);
 
 export default routes;
