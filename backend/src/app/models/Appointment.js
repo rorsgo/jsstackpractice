7b22a5c2b6beb0e07a03120107aafc9965bab7bf
rorvts <rorvts@gmail.com>
2020-09-24 15:02:59 +0200

    Adding cancelable and past fields to the appointments list

--- backend/src/app/models/Appointment.js
+++ backend/src/app/models/Appointment.js
@@ -1,3 +1,4 @@
+import { isBefore, subHours } from "date-fns";
 import Sequelize, { Model } from "sequelize";
 
 class Appointment extends Model {
@@ -5,7 +6,19 @@ class Appointment extends Model {
     super.init(
       {
         date: Sequelize.DATE,
-        canceled_at: Sequelize.DATE
+        canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date())
+          }
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2))
+          }
+        }
       },
       {
         sequelize
@@ -16,8 +29,8 @@ class Appointment extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.User, { foreignKey: "user_id", as: "user"});
-    this.belongsTo(models.User, { foreignKey: "provider_id", as: "provider"})
+    this.belongsTo(models.User, { foreignKey: "user_id", as: "user" });
+    this.belongsTo(models.User, { foreignKey: "provider_id", as: "provider" })
   }
 }
 
