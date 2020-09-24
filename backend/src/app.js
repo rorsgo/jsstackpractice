a61418901e07e6a11c2366424f6fa8c848bcf093
rorvts <rorvts@gmail.com>
2020-09-24 17:24:31 +0200

    Handling errors with sentry and youch

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,46 @@
-import express from "express";
-import routes from "./routes";
 import { resolve } from "path";
+import express from "express";
+import "express-async-errors";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+import Youch from "youch";
+
+import routes from "./routes";
 import "./database";
 
 class App {
-  constructor(){
+  constructor() {
     this.server = express();
-
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express(this.server),
+      ],
+      tracesSampleRate: process.env.SENTRY_TRACERATE,
+    });
     this.middleware();
     this.routes();
+    this.exceptionHandler();
   }
 
-  middleware(){
+  middleware() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use("/files", express.static(resolve(__dirname, "..", "tmp", "uploads")));
   }
 
-  routes(){
+  routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (error, request, response, next) => {
+      const errors = await new Youch(error, request).toJSON();
+
+      return response.status(500).json(errors);
+    });
   }
 }
 
