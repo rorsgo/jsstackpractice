6ae700364dff1df78ff0d86f09651079c5108121
rorvts <rorvts@gmail.com>
2020-11-09 09:27:51 +0100

    Initializing react with redux application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Routes from "./routes";
-import GlobalStyles from "./styles/global";
 
 const App = () => {
   return (
     <>
-      <Routes />
-      <GlobalStyles />
+      <h1>Applying Redux</h1>
     </>
   )
 }
