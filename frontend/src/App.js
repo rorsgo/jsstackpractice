7c894520625f683e2886276e29e9cc96ee854b10
rorvts <rorvts@gmail.com>
2020-11-09 09:58:59 +0100

    Application global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import GlobalStyle from "./styles/global";
+import Routes from "./routes";
 
 const App = () => {
   return (
     <>
-      <h1>Applying Redux</h1>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes />
+      </BrowserRouter>
     </>
   )
 }
