56e46c72debaa053420735fadab87da446bf5726
rorvts <rorvts@gmail.com>
2020-11-11 12:52:12 +0100

    Redux configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import GlobalStyle from "./styles/global";
 import Routes from "./routes";
+import store from "./store";
 
 import Header from "./components/Header";
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
         <Header />
         <GlobalStyle />
         <Routes />
       </BrowserRouter>
-    </>
+    </Provider>
   )
 }
 
