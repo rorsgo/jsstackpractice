9a9b64a6a56d5e2f29294f25214bafd038370893
rorvts <rorvts@gmail.com>
2020-11-11 12:41:11 +0100

    JSON Server api and Intl format on api data

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,95 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
+import api from "../../services/api";
+import { formatPrice } from "../../util/format";
 
 import { ProductList } from "./styles";
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1200,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/0204/0483/0283/products/Allbirds_WL_RN_SF_PDP_Natural_Black_BTY_fc7cfd13-f8fb-4723-a478-a2fea603c9fe.png?v=1600891285"
-          alt="sneaker"
-        />
-        <strong>Nice Sneaker</strong>
-        <span>$10.90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1200,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/0204/0483/0283/products/Allbirds_WL_RN_SF_PDP_Natural_Black_BTY_fc7cfd13-f8fb-4723-a478-a2fea603c9fe.png?v=1600891285"
-          alt="sneaker"
-        />
-        <strong>Nice Sneaker</strong>
-        <span>$10.90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1200,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/0204/0483/0283/products/Allbirds_WL_RN_SF_PDP_Natural_Black_BTY_fc7cfd13-f8fb-4723-a478-a2fea603c9fe.png?v=1600891285"
-          alt="sneaker"
-        />
-        <strong>Nice Sneaker</strong>
-        <span>$10.90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1200,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/0204/0483/0283/products/Allbirds_WL_RN_SF_PDP_Natural_Black_BTY_fc7cfd13-f8fb-4723-a478-a2fea603c9fe.png?v=1600891285"
-          alt="sneaker"
-        />
-        <strong>Nice Sneaker</strong>
-        <span>$10.90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1200,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/0204/0483/0283/products/Allbirds_WL_RN_SF_PDP_Natural_Black_BTY_fc7cfd13-f8fb-4723-a478-a2fea603c9fe.png?v=1600891285"
-          alt="sneaker"
-        />
-        <strong>Nice Sneaker</strong>
-        <span>$10.90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1200,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/0204/0483/0283/products/Allbirds_WL_RN_SF_PDP_Natural_Black_BTY_fc7cfd13-f8fb-4723-a478-a2fea603c9fe.png?v=1600891285"
-          alt="sneaker"
-        />
-        <strong>Nice Sneaker</strong>
-        <span>$10.90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-    </ProductList>
-  )
+export default class Home extends Component {
+  state = {
+    products: []
+  }
+
+  async componentDidMount() {
+    const response = await api.get("products");
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img
+              src={product.image}
+              alt={product.title}
+            />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+              <span>Add to cart</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    )
+  }
 }
