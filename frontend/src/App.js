dca2e099218e924db8d32a1515ca5d3cafc6f994
rorvts <rorvts@gmail.com>
2020-11-19 19:40:36 +0100

    Frontend reset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,9 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { ToastContainer } from "react-toastify";
-
-import "./config/ReactotronConfig";
-import GlobalStyle from "./styles/global";
-import Routes from "./routes";
-import store from "./store";
-
-import Header from "./components/Header";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <GlobalStyle />
-        <ToastContainer autoClose={3000}/>
-        <Routes />
-      </BrowserRouter>
-    </Provider>
-  )
+    <></>
+  );
 }
 
 export default App;
