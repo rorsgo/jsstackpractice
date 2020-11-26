5b2751da4e5157ab6c646e4eeff1896df66f30a1
rorvts <rorvts@gmail.com>
2020-11-26 10:41:17 +0100

    GlobalStyles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Router } from "react-router-dom";
 
+import GlobalStyles from "./styles/global";
 import "./config/ReactotronConfig";
 import Route from "./routes";
 import history from "./services/history";
@@ -8,6 +9,7 @@ import history from "./services/history";
 const App = () => {
   return (
     <Router history={history}>
+      <GlobalStyles />
       <Route />
     </Router>
   );
--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -2,5 +2,5 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     height: 100%;
-    background: linear-gradient(-90deg, #191920, #22102F)
+    background: linear-gradient(-90deg, #221920, #09102F)
 `;
