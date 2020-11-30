f36171de53aa33af960a63ee48d2c46610a6ca21
rorvts <rorvts@gmail.com>
2020-11-30 13:20:37 +0100

    Redux ReduxSaga and Reactotron config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 
 import GlobalStyles from "./styles/global";
 import "./config/ReactotronConfig";
 import Route from "./routes";
 import history from "./services/history";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router history={history}>
-      <GlobalStyles />
-      <Route />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyles />
+        <Route />
+      </Router>
+    </Provider>
   );
 }
 
